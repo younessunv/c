67ef1339463580f337972a2b13ad7faecde76e33
younessunv <125516552+younessunv@users.noreply.github.com>
2023-12-06 08:31:47 +0000

    Create passwordGenerator.c

---
+++ practice/passwordGenerator.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAX_LENGTH 30
+
+void passwordGenerater(char *password, int length);
+
+int main()
+{
+    int length;
+
+    printf("----------------insert -1 to leave.----------------");
+    while(1)
+    {
+        do{
+            printf("\ninsert how many characters you want in your password: ");
+            scanf("%d", &length);
+        }while(length <= 0 || length > MAX_LENGTH);
+
+        if(length == -1)
+            break;
+
+        char password[length + 1];
+        passwordGenerater(password, length);
+        printf("%s", password);
+    }
+    
+    return 0;
+}
+
+void passwordGenerater(char *password, int length) 
+{
+    srand(time(NULL));
+    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@$&*?";
+
+    for(int i = 0; i < length; i++)
+    {
+        char randomIndex = rand() % (sizeof(charset) - 1);
+        password[i] = charset[randomIndex];
+    }
+
+    password[length] = '\0';
+
+}
