7a60082a8b7e044c4d3ab2929289824fe93a9738
younessunv <125516552+younessunv@users.noreply.github.com>
2023-12-06 13:45:12 +0000

    Update passwordGenerator.c

--- practice/passwordGenerator.c
+++ practice/passwordGenerator.c
@@ -1,55 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdbool.h>
 
 #define MAX_LENGTH 30
+#define EXIT_CODE -1
 
 void passwordGenerater(char *password, int length);
 
 int main()
 {
+    srand(time(NULL));
     int length, attempts = 0;
+    bool continueInput = true;
 
     printf("----------------insert -1 to leave.----------------");
-    while(1)
+    do
     {
-        do{
-            printf("\ninsert how many characters you want in your password: ");
-            scanf("%d", &length);
-        }while(length <= 0 || length > MAX_LENGTH);
+        printf("\nInsert how many characters you want in your password: ");
+        scanf("%d", &length);
 
-        if(length == -1)
-            break;
-
-        char password[length + 1];
-        passwordGenerater(password, length);
-        printf("%s\n", password);
-        attempts++;
-
-        
-        if(attempts == 3)
+        if (length == EXIT_CODE)
         {
-            system("pause");
-            system("cls");
-            printf("----------------insert -1 to leave.----------------");
-            attempts = 0;
+            continueInput = false;
         }
-    }
-    
+        else if (length <= 0 || length > MAX_LENGTH)
+        {
+            printf("Invalid input. Length should be between 1 and %d.\n", MAX_LENGTH);
+        }
+        else
+        {
+            char password[length + 1];
+            passwordGenerater(password, length);
+            printf("%s\n", password);
+            attempts++;
+
+            if (attempts == 3)
+            {
+                system("pause");
+                system("cls");
+                printf("----------------Insert -1 to leave.----------------");
+                attempts = 0;
+            }
+        }
+    } while (continueInput);
+
     return 0;
 }
 
-void passwordGenerater(char *password, int length) 
+void passwordGenerater(char *password, int length)
 {
-    srand(time(NULL));
     const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@$&*?";
 
-    for(int i = 0; i < length; i++)
+    for (int i = 0; i < length; i++)
     {
         char randomIndex = rand() % (sizeof(charset) - 1);
         password[i] = charset[randomIndex];
     }
 
     password[length] = '\0';
-
 }
