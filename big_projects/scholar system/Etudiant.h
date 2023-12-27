b25018088fddb4908d5a77d2b2bfa5edcf8740c3
younessunv <125516552+younessunv@users.noreply.github.com>
2023-12-27 12:05:15 +0000

    Update Etudiant.h

--- big_projects/scholar system/Etudiant.h
+++ big_projects/scholar system/Etudiant.h
@@ -10,11 +10,13 @@ struct Etudiant
     char Etudiant_formation[33];
 };
 
-
 void get_Etudiant_Info(struct Etudiant *Etudiant);
 void adding_Etudiant_info(const struct Etudiant *Etudiant, int personCount);
 int read_Etudiant_Count();
 void write_etudiant_Count(int count);
 void check_Etudiant_Info_by_name(const char *firstName, const char *lastName);
+void check_Etudiant_Info_by_age(const int age);
+void check_Etudiant_Info_by_city(const char *city);
+void check_Etudiant_Info_by_formation(const char *formation);
 
 #endif
