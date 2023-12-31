3a3090c655b409cf0a4deb70a537d6b739cdf085
younessunv <younessstudies@gmail.com>
2023-12-31 21:16:40 +0000

    add header files to scholar system

---
+++ big_projects/scholar system/Formation.h
@@ -0,0 +1,18 @@
+#ifndef FORMATION_H
+#define FORMATION_H
+
+
+typedef struct Formation
+{
+    char Designation[33];
+    char Modules[6][33];
+};
+
+
+
+
+
+
+
+
+#endif
---
+++ big_projects/scholar system/Teacher.h
@@ -0,0 +1,11 @@
+#ifndef TEACHER_H
+#define TEACHER_H
+
+typedef struct Teacher
+{
+    char Teacher_First_name[33];
+    char Teacher_Last_name[33];
+    char Teacher_speciality[33];
+};
+
+#endif
